bd3623f1bbc4251054e13392385d5a4d891f010e
thedmvdevaustin <thedmvdevaustin@gmail.com>
2025-06-13 11:53:04 -0400

    added another solution to #1046 Last Stone Weight

--- 1046-lastStoneWeight.js
+++ 1046-lastStoneWeight.js
@@ -57,3 +57,80 @@ Space Algorithm: depending on the sorting algorithm used can be n log n or n^2 a
 case;
 */
 
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function(stones) {
+    let heap = new MaxHeap();
+    stones.forEach(x => heap.push(x));
+    while (heap.size() > 1) {
+        let difference = heap.pop() - heap.pop();
+        if (difference) {
+            heap.push(difference)
+        }
+    }
+    return heap.size() ? heap.pop() : 0;
+};
+
+class MaxHeap {
+    constructor() {
+        this.heap = [null];
+    }
+    //size, push, pop
+    size() {
+        return this.heap.length - 1;
+    }
+
+    push(value) {
+        this.heap.push(value);
+        let index = this.size(), element = this.heap[index];
+        while (index > 1) {
+            let parentIndex = Math.floor(index / 2);
+            let parentElement = this.heap[parentIndex];
+            if (element <= parentElement) break;
+            this.heap[index] = parentElement;
+            index = parentIndex;
+        }
+        this.heap[index] = element;
+    }
+
+    pop() {
+        if (this.size()===1) return this.heap.pop();
+        const root = this.heap[1];
+        this.heap[1] = this.heap.pop();
+        let index = 1, element = this.heap[index];
+        while (true) {
+            let leftChildIndex = 2 * index, rightChildIndex = 2 * index + 1;
+            let leftChildElement, rightChildElement;
+            let swap = null;
+            if (leftChildIndex <= this.size()) {
+                leftChildElement = this.heap[leftChildIndex];
+                if (leftChildElement > element) {
+                    swap = leftChildIndex;
+                }
+            }
+            if (rightChildIndex <= this.size()) {
+                rightChildElement = this.heap[rightChildIndex];
+                if ((swap===null && rightChildElement > element) || (swap!==null && rightChildElement > leftChildElement)) {
+                    swap = rightChildIndex;
+                }
+            }
+            if (swap===null) break;
+            this.heap[index] = this.heap[swap];
+            index = swap;
+        }
+        this.heap[index] = element;
+        return root;
+    }
+
+}
+
+/*
+Time Complexity: O(N log N); worst case we have to traverse the array of stones
+and put each element inside the Heap. worst case in the while loop we are
+popping 2 elements and adding one every time which will iterate the stones
+length amount of times but inside the loop we are performing log N operations
+so the result ends up being O(N) + O(N log N) which is O(N log N);
+Space Complexity: O(N) for adding every element into the heap;
+*/
