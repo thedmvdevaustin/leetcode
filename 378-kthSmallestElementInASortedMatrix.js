090f6638f26adc01053740d0ccb0546d879e61ce
thedmvdevaustin <thedmvdevaustin@gmail.com>
2025-06-16 17:40:50 -0400

    first solution to #378 Kth Smallest Element in a Sorted Matrix

---
+++ 378-kthSmallestElementInASortedMatrix.js
@@ -0,0 +1,114 @@
+// Given an n x n matrix where each of the rows and columns is sorted in ascending order, return the kth smallest element in the matrix.
+
+// Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+
+// You must find a solution with a memory complexity better than O(n2).
+
+ 
+
+// Example 1:
+
+// Input: matrix = [[1,5,9],[10,11,13],[12,13,15]], k = 8
+// Output: 13
+// Explanation: The elements in the matrix are [1,5,9,10,11,12,13,13,15], and the 8th smallest number is 13
+// Example 2:
+
+// Input: matrix = [[-5]], k = 1
+// Output: -5
+ 
+
+// Constraints:
+
+// n == matrix.length == matrix[i].length
+// 1 <= n <= 300
+// -109 <= matrix[i][j] <= 109
+// All the rows and columns of matrix are guaranteed to be sorted in non-decreasing order.
+// 1 <= k <= n2
+ 
+
+// Follow up:
+
+// Could you solve the problem with a constant memory (i.e., O(1) memory complexity)?
+// Could you solve the problem in O(n) time complexity? The solution may be too advanced for an interview but you may find reading this paper fun.
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(matrix, k) {
+    let heap = new MinHeap()
+    matrix.forEach(row => {
+        row.forEach(x => {
+            heap.push(x)
+        })
+    })
+    while (k>1) {
+        heap.delete()
+        k--;
+    }
+    return heap.delete()
+};
+
+class MinHeap {
+    constructor() {
+        this.heap = [null];
+    }
+
+    size() {
+        return this.heap.length - 1;
+    }
+
+    peek() {
+        return this.heap[1];
+    }
+
+    push(value) {
+        this.heap.push(value);
+        let index = this.size(), element = this.heap[index];
+        while (index > 1) {
+            let parentIndex = Math.floor(index/2);
+            let parentElement = this.heap[parentIndex];
+            if (element >= parentElement) break;
+            this.heap[index] = parentElement;
+            index = parentIndex;
+        }
+        this.heap[index] = element;
+    }
+
+    delete() {
+        if (this.size()===1) return this.heap.pop();
+        const root = this.peek();
+        this.heap[1] = this.heap.pop();
+        let index = 1, element = this.heap[index];
+        while (true) {
+            let leftChildIndex = 2*index, rightChildIndex = 2*index+1;
+            let leftChildElement, rightChildElement;
+            let swap = null;
+            if (leftChildIndex <= this.size()) {
+                leftChildElement = this.heap[leftChildIndex];
+                if (leftChildElement < element) {
+                    swap = leftChildIndex;
+                }
+                if (rightChildIndex <= this.size()) {
+                    rightChildElement = this.heap[rightChildIndex];
+                    if ((swap === null && rightChildElement < element) || (swap!==null && rightChildElement < leftChildElement)) {
+                        swap = rightChildIndex;
+                    }
+                }
+            }
+            if (swap===null) break;
+            this.heap[index] = this.heap[swap];
+            index = swap;
+        }
+        this.heap[index] = element;
+        return root;
+    }
+}
+
+/*
+Time Complexity: O((N log N) + k): to put every element in the matrix is n log n(n
+to search every row and log n to add the row to the heap) and k to get the the kth
+minimum element;
+Space Complexity: O(n^2); puttting every element in the matrix inside the heap
+*/
