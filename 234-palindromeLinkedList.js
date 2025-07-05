6fb79fb34d7c2b637c790745aada5888b0e76e29
thedmvdevaustin <thedmvdevaustin@gmail.com>
2025-07-05 09:36:11 -0400

    second solution to #234 Palindrome Linked List

--- 234-palindromeLinkedList.js
+++ 234-palindromeLinkedList.js
@@ -74,6 +74,40 @@ var isPalindrome = function(head) {
     return true;
 };
 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+
+var isPalindrome = function(head) {
+    if (!head.next) return head;
+    let slow = head, fast = head, prev = null;
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        let next = slow.next;
+        slow.next = prev;
+        prev = slow;
+        slow = next;
+    }
+    if (fast) slow = slow.next;
+    while (slow && prev) {
+        if (slow.val !== prev.val) {
+            return false;
+        }
+        slow = slow.next;
+        prev = prev.next;
+    }
+    return true;
+};
+
 /*
 Time Complexity: O(N): worst case scenario we are traversing the linked list
 O(N/2) every time we traverse which simplifies down to O(N);
