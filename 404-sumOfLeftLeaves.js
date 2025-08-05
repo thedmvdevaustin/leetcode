fdafd5f1138263a20ea0d4956499145b083c0b5a
thedmvdevaustin <thedmvdevaustin@gmail.com>
2025-08-05 02:40:03 -0400

    first solution to #404 Sum Of Left Leaves

---
+++ 404-sumOfLeftLeaves.js
@@ -0,0 +1,57 @@
+// Given the root of a binary tree, return the sum of all left leaves.
+
+// A leaf is a node with no children. A left leaf is a leaf that is the left child of another node.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: 24
+// Explanation: There are two left leaves in the binary tree, with values 9 and 15 respectively.
+// Example 2:
+
+// Input: root = [1]
+// Output: 0
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 1000].
+// -1000 <= Node.val <= 1000
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function(root) {
+    let queue = [[root, false]], total = 0;
+    while (queue.length) {
+        let length = queue.length;
+        for (let i = 0; i < length; i++) {
+            let [ node, isLeft ] = queue.shift();
+            if (isLeft && !node.left && !node.right) total+=node.val;
+            if (node.left) queue.push([node.left, true]);
+            if (node.right) queue.push([node.right, false]);
+        }
+    }
+    return total;
+};
+
+/*
+Time Complexity: O(N); worst case scenario we are traversing the entire 
+tree and each iteration performing constant operations so this simplifies
+down to O(N);
+Space Complexity: O(N); worst case scenario we have the bottom level filled
+completely with nodes in which we will push n/2 nodes to the queue which
+simplifies down to O(N);
+*/
