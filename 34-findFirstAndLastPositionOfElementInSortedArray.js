24b70d8bee757906add4ecac0656d092bebe40a2
thedmvdevaustin <thedmvdevaustin@gmail.com>
2025-09-19 10:09:48 -0400

    first solution to #34 Find First And Last Position Of Element In Sorted Array

---
+++ 34-findFirstAndLastPositionOfElementInSortedArray.js
@@ -0,0 +1,62 @@
+// Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+// If target is not found in the array, return [-1, -1].
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+// Example 1:
+
+// Input: nums = [5,7,7,8,8,10], target = 8
+// Output: [3,4]
+// Example 2:
+
+// Input: nums = [5,7,7,8,8,10], target = 6
+// Output: [-1,-1]
+// Example 3:
+
+// Input: nums = [], target = 0
+// Output: [-1,-1]
+ 
+
+// Constraints:
+
+// 0 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+// nums is a non-decreasing array.
+// -109 <= target <= 109
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    function binarySearch(forLeft) {
+        let left = 0, right = nums.length - 1, res = -1;
+        while (left <= right) {
+            let mid = Math.floor((left+right)/2);
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1
+            } else {
+                res = mid;
+                forLeft ? right = mid - 1 : left = mid + 1;
+            }
+        }
+        return res
+    }
+
+    left = binarySearch(true);
+    right = binarySearch(false);
+    return [left, right];
+};
+
+/*
+Time Complexity: O(log n); worst case scenario we have to traverse the 
+array 2 separate times; each taking log n which is O(log n + log n) which
+simplifies down to O(log n);
+Space Complexity: O(1); no additional space is needed
+*/
