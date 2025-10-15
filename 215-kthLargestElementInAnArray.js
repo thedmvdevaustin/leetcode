461f27505ca939c53401c9989a9f63fdb5b32b72
thedmvdevaustin <thedmvdevaustin@gmail.com>
2025-10-15 14:51:26 -0400

    added second solution to #215 Kth Largest Element In An Array

--- 215-kthLargestElementInAnArray.js
+++ 215-kthLargestElementInAnArray.js
@@ -94,8 +94,91 @@ class MaxHeap {
 }
 
 /*
-Time Complexity: O(N); worst case scenario k is equal to nums.length
+Time Complexity: O(N+K); worst case scenario k is equal to nums.length
 and we have to pop every element off of the heap to get out solution
 Space Complexity: O(N); we make a heap and put every element onto the heap
+*/
+//SECOND SOLUTION SLIGHTLY MORE OPTIMIZED
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    let minHeap = new MyMinHeap();
+    for (let i = 0; i < nums.length; i++){
+        minHeap.add(nums[i]);
+        if (minHeap.size() > k) {
+            minHeap.delete();
+        }
+    }
+    return minHeap.peek();
+};
 
+class MyMinHeap {
+    constructor() {
+        this.heap = [null];
+    }
+
+    //size, peek, add, delete
+    size() {
+        return this.heap.length - 1;
+    }
+
+    peek() {
+        return this.heap[1];
+    }
+
+    add(value) {
+        this.heap.push(value);
+        let index = this.size(), element = this.heap[index];
+        while (index > 1) {
+            let parentIndex = Math.floor(index/2), parentElement = this.heap[parentIndex];
+            if (element >= parentElement) break;
+            this.heap[index] = parentElement;
+            index = parentIndex;
+        }
+        this.heap[index] = element;
+        return;
+    } 
+
+    delete() {
+        if (this.size()===1) return this.heap.pop();
+        const root = this.peek();
+        this.heap[1] = this.heap.pop();
+        let index = 1, element = this.heap[index];
+        while (true) {
+            let leftChildIndex = index * 2, rightChildIndex = index * 2 + 1;
+            let leftChildElement, rightChildElement;
+            let swap = null;
+            if (leftChildIndex <= this.size()) {
+                leftChildElement = this.heap[leftChildIndex];
+                if (leftChildElement < element) {
+                    swap = leftChildIndex;
+                }
+                if (rightChildIndex <= this.size()) {
+                    rightChildElement = this.heap[rightChildIndex];
+                    if ((swap!==null && rightChildElement < leftChildElement) || (swap===null && rightChildElement < element)) {
+                        swap = rightChildIndex
+                    }
+                }
+            }
+            if (swap===null) break;
+            this.heap[index] = this.heap[swap];
+            index = swap;
+        }
+        this.heap[index] = element;
+        return root;
+    }
+}
+
+/*
+Time Complexity: O(N log k); esentially we are looping through the array and only keeping
+k amount of elements inside our heap at once(technically k+1) so whenever we perform our 
+operations on the heap the time complexity is O(log k) instead of O(log n) like how it is in 
+the first solution; since we perform a log k operation each time (twice separately after we
+reach k amount of elements in the heap which is log k + log k which simplifies down to log k)
+the time complexity simplifies down to O(N log K);
+Space Complexity: O(log k); we will only have O(log k+1) items in the heap at a time which 
+simplifies down to O(log k); 
 */
