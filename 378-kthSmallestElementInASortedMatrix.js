9396e33379b94ff05079fe2076bdd9b191b25c75
thedmvdevaustin <thedmvdevaustin@gmail.com>
2025-10-18 12:38:26 -0400

    added second solution to #378 Kth Smallest Element In A Sorted Matrix

--- 378-kthSmallestElementInASortedMatrix.js
+++ 378-kthSmallestElementInASortedMatrix.js
@@ -112,3 +112,91 @@ to search every row and log n to add the row to the heap) and k to get the the k
 minimum element;
 Space Complexity: O(n^2); puttting every element in the matrix inside the heap
 */
+
+//SECOND SOLUTION
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(matrix, k) {
+    let output = [], minHeap = new MyMinHeap();
+    matrix.forEach((x, i) => {
+        minHeap.queue([x[0], i, 0])
+    })
+    console.log(minHeap);
+    while (k && minHeap.size()) {
+        let [number, array, index] = minHeap.dequeue();
+        output.push(number);
+        if (index+1 < matrix[array].length) {
+            minHeap.queue([matrix[array][index+1], array, index+1]);
+        }
+        k--;
+    }
+    return output[output.length - 1];
+};
+
+class MyMinHeap {
+    constructor() {
+        this.heap = [null];
+    }
+
+    size() {
+        return this.heap.length - 1;
+    }
+
+    peek() {
+        return this.heap[1];
+    }
+
+    queue(value) {
+        this.heap.push(value);
+        let index = this.size(), element = this.heap[index];
+        while (index > 1) {
+            let parentIndex = Math.floor(index/2);
+            let parentElement = this.heap[parentIndex];
+            if (element[0] >= parentElement[0]) break;
+            this.heap[index] = parentElement;
+            index = parentIndex;
+        }
+        this.heap[index] = element;
+    }
+
+    dequeue() {
+        if (this.size()===1) return this.heap.pop();
+        const root = this.peek();
+        this.heap[1] = this.heap.pop();
+        let index = 1, element = this.heap[index];
+        while (true) {
+            let leftChildIndex = 2*index, rightChildIndex = 2*index+1;
+            let leftChildElement, rightChildElement;
+            let swap = null;
+            if (leftChildIndex <= this.size()) {
+                leftChildElement = this.heap[leftChildIndex];
+                if (leftChildElement[0] < element[0]) {
+                    swap = leftChildIndex;
+                }
+                if (rightChildIndex <= this.size()) {
+                    rightChildElement = this.heap[rightChildIndex];
+                    if ((swap === null && rightChildElement[0] < element[0]) || (swap!==null && rightChildElement[0] < leftChildElement[0])) {
+                        swap = rightChildIndex;
+                    }
+                }
+            }
+            if (swap===null) break;
+            this.heap[index] = this.heap[swap];
+            index = swap;
+        }
+        this.heap[index] = element;
+        return root;
+    }
+}
+
+/*
+Time Complexity: O(K Log N); we have 2 loops, the first one will go through M times(M being 
+the number of arrays we are dealing with) with each loop only completing log M operations
+and the second loop will iterate K times max with each time performing log N operations; this
+simplifies down to O(K log N) since that is the bigger time complexity;
+Space Complexity: O(Log K); worst case scenario only K elements will be stored inside the array
+*/
