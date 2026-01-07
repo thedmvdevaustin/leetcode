fbd3035f0595db23c090b02306f54649f8de9832
thedmvdevaustin <thedmvdevaustin@gmail.com>
2026-01-07 18:25:03 -0500

    first solution to #857 Minimum Cost To Hire K Workers

---
+++ 857-minimumCostToHireKWorkers.js
@@ -0,0 +1,110 @@
+// There are n workers. You are given two integer arrays quality and wage where quality[i] is the quality of the ith worker and wage[i] is the minimum wage expectation for the ith worker.
+
+// We want to hire exactly k workers to form a paid group. To hire a group of k workers, we must pay them according to the following rules:
+
+// Every worker in the paid group must be paid at least their minimum wage expectation.
+// In the group, each worker's pay must be directly proportional to their quality. This means if a worker’s quality is double that of another worker in the group, then they must be paid twice as much as the other worker.
+// Given the integer k, return the least amount of money needed to form a paid group satisfying the above conditions. Answers within 10-5 of the actual answer will be accepted.
+
+ 
+
+// Example 1:
+
+// Input: quality = [10,20,5], wage = [70,50,30], k = 2
+// Output: 105.00000
+// Explanation: We pay 70 to 0th worker and 35 to 2nd worker.
+// Example 2:
+
+// Input: quality = [3,1,10,10,1], wage = [4,8,2,2,7], k = 3
+// Output: 30.66667
+// Explanation: We pay 4 to 0th worker, 13.33333 to 2nd and 3rd workers separately.
+ 
+
+// Constraints:
+
+// n == quality.length == wage.length
+// 1 <= k <= n <= 104
+// 1 <= quality[i], wage[i] <= 104
+
+/**
+ * @param {number[]} quality
+ * @param {number[]} wage
+ * @param {number} k
+ * @return {number}
+ */
+var mincostToHireWorkers = function(quality, wage, k) {
+    let array = Array.from({ length: quality.length}, (_, i) => [wage[i]/quality[i], quality[i]]).sort((a,b) => a[0]-b[0]);
+    let total = 0, answer = Infinity, maxHeap = new MyMaxHeap();
+    for (const [ratio, qual] of array) {
+        maxHeap.enqueue(qual);
+        total+=qual;
+        if (maxHeap.size() > k) {
+            total-=maxHeap.dequeue();
+        }
+        if (maxHeap.size() === k ) {
+            answer = Math.min(answer, total*ratio);
+        }
+    }
+    return answer;
+};
+
+class MyMaxHeap {
+    constructor() {
+        this.heap = [null];
+    }
+
+    size() {
+        return this.heap.length - 1;
+    }
+
+    peek() {
+        return this.heap[1];
+    }
+
+    enqueue(value) {
+        this.heap.push(value);
+        let index = this.size(), element = this.heap[index];
+        while (index > 1) {
+            let parentIndex = Math.floor(index/2), parentElement = this.heap[parentIndex];
+            if (element <= parentElement) break;
+            this.heap[index] = parentElement;
+            index = parentIndex;
+        }
+        this.heap[index] = element;
+    }
+
+    dequeue() {
+        if (this.size()===1) return this.heap.pop();
+        const root = this.peek();
+        this.heap[1] = this.heap.pop();
+        let index = 1, element = this.heap[index];
+        while (true) {
+            let leftChildIndex = 2*index, rightChildIndex = 2*index+1;
+            let leftChildElement, rightChildElement;
+            let swap = null;
+            if (leftChildIndex <= this.size()) {
+                leftChildElement = this.heap[leftChildIndex];
+                if (leftChildElement > element) {
+                    swap = leftChildIndex;
+                }
+                if (rightChildIndex <= this.size()) {
+                    rightChildElement = this.heap[rightChildIndex];
+                    if ((swap===null && rightChildElement > element) || (swap!==null && rightChildElement > leftChildElement)) {
+                        swap = rightChildIndex;
+                    }
+                }
+            }
+            if (swap===null) break;
+            this.heap[index] = this.heap[swap];
+            index = swap
+        }
+        this.heap[index] = element;
+        return root;
+    }
+}
+
+/*
+Time Complexity: O(n log n); for the sorting algorithm;
+Space Complexity: O(N); at worst case we will have n elements in the created array and n 
+elements in the heap at once
+*/
